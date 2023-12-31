a2684ca5685bc609dd7e064280cc84dc91f38af2
KevinMuniz1 <munizkevin@outlook.com>
2023-12-31 15:48:52 -0500

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Kevin Muniz on 12/30/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var costOfCheck: Double = 0
+    @State private var tipPercentage: Double = 0
+    @State private var amountOfPeople: Double = 0
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var body: some View {
+        Form {
+            Section{
+                TextField("Enter cost of check", value: $costOfCheck, format: .currency(code: "USD"))
+            }
+        }
+    }
+}
+#Preview {
+    ContentView()
+}
---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Kevin Muniz on 12/30/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
