d99349677c04f215c4753f1cf0067ac52f58fe91
KevinMuniz1 <munizkevin@outlook.com>
2023-12-31 16:30:30 -0500

    Added the functionality of a picker in order to select the amount of people that are splitting the check

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,20 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var costOfCheck: Double = 0
-    @State private var tipPercentage: Double = 0
-    @State private var amountOfPeople: Double = 0
+    @State private var tipPercentage = 0
+    @State private var amountOfPeople = 2
     
     let tipPercentages = [10, 15, 20, 25, 0]
     
     var body: some View {
-        Form {
-            Section{
-                TextField("Enter cost of check", value: $costOfCheck, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-                    .keyboardType(.decimalPad)
-            }
-            Section{
-                Text(costOfCheck, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-            }
+        NavigationStack{
+            Form {
+                Section{
+                    TextField("Enter cost of check", value: $costOfCheck, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number of people", selection: $amountOfPeople){
+                        ForEach(2..<17){
+                            Text("\($0) people")
+                        }
+                    }
+                    Picker("Enter tip ammount", selection: $tipPercentage){
+                        
+                    }
+                }
+            }.navigationTitle("We Split")
         }
     }
 }
