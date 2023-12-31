7e9d878ccee35dced15185042d9677972b2d673d
KevinMuniz1 <munizkevin@outlook.com>
2023-12-31 17:31:18 -0500

    Implented the calculations to find the total amount per person and using a toolbar to dismiss the keyboard when done inputting a value

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -11,15 +11,26 @@ struct ContentView: View {
     @State private var costOfCheck: Double = 0
     @State private var tipPercentage = 0
     @State private var amountOfPeople = 2
+    @FocusState private var isFocused: Bool
+    
+    private var finalAmount: Double {
+        let numberOfPeople = Double(amountOfPeople + 2)
+        let tipAmount = Double(tipPercentage)
+        
+        let tipValue = costOfCheck / 100 * tipAmount
+        let grandTotal = costOfCheck + tipValue
+        let amountPerPerson = grandTotal / numberOfPeople
+        return amountPerPerson
+    }
     
     let tipPercentages = [10, 15, 20, 25, 0]
-    
-    var body: some View {
+        var body: some View {
         NavigationStack{
             Form {
                 Section{
                     TextField("Enter cost of check", value: $costOfCheck, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                         .keyboardType(.decimalPad)
+                        .focused($isFocused)
                     
                     Picker("Number of people", selection: $amountOfPeople){
                         ForEach(2..<17){
@@ -34,7 +45,17 @@ struct ContentView: View {
                             }
                         }.pickerStyle(.segmented)
                     }
+                Section("Final Amount") {
+                    Text(finalAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
                 }.navigationTitle("We Split")
+                .toolbar {
+                    if isFocused {
+                        Button("Done"){
+                            isFocused = false
+                        }
+                    }
+                }
             }
         }
     }
